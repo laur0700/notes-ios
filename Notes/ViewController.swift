152d5156dea324d36708866e47f1889382c7cedd
laur0700 <laur.postl@gmail.com>
2022-05-08 02:49:22 +0300

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Laurentiu-Andrei Postole on 08.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
